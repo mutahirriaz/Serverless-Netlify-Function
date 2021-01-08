0773f5b1235de61bdaf91d3aab304ca6e522ffa6
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-01-08 22:01:43 +0500

    netlify commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    (async() => {
+      const response = await fetch('.netlify/functions/hello')
+      const tempData = await response.json();
+      setData(tempData)
+    }
+    )();
+  }, [])
+
+  return (
+    <div>
+      <h1>Serverless Netlify Function</h1>
+      {data.message}
+    </div>
+  )
 }
